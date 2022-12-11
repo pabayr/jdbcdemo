7dbbc0b2c910e8a440cf129d8abc80b8d08fe638
pabayrgit <pat.bayr@gmail.com>
2022-12-11 22:48:02 +0100

    delete, findallbyname like, fertig

--- src/main/java/JdbcDemo.java
+++ src/main/java/JdbcDemo.java
@@ -6,9 +6,63 @@ public class JdbcDemo {
         selectAllDemo();
        updateStudentDemo();
        selectAllDemo();
+       deleteStudentDemo(3);
+       selectAllDemo();
+       findAllByNameLike("ba");
 
     }
 
+    public static void findAllByNameLike(String sname)
+    {
+        System.out.println("Select DEMO mit JDBC");
+        String connectionURL="jdbc:mysql://localhost:3306/jdbcdemo";
+        String user= "root";
+        String pwd="";
+        try(Connection conn = DriverManager.getConnection(connectionURL,user,pwd)){
+            System.out.println("Verbindung zur DB hergestellt");
+
+            PreparedStatement preparedStatement= conn.prepareStatement("SELECT * FROM `student` WHERE `student`.`name` LIKE ?");
+            preparedStatement.setString(1,"%"+sname+"%");
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next())
+            {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email= rs.getString("email");
+                System.out.println("Student aus der DB: [ID] " + id + " [NAME] " + name + " [EMAIL] " + email);
+            }
+        }catch(SQLException e)
+        {
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+
+
+    }
+    public static void deleteStudentDemo(int studentID) //Hier geben wir den Parameter gleich mit anstatt sie zu hardcoden
+    {
+        System.out.println("Delete DEMO");
+        String connectionURL="jdbc:mysql://localhost:3306/jdbcdemo";
+        String user= "root";
+        String pwd="";
+        try(Connection conn = DriverManager.getConnection(connectionURL,user,pwd)){
+            System.out.println("Verbindung zur DB hergestellt");
+
+            PreparedStatement preparedStatement= conn.prepareStatement("DELETE FROM `student` where `student`.`id` = ?;");
+            try {
+                preparedStatement.setInt(1,studentID);
+                int affectedRows=preparedStatement.executeUpdate();
+                System.out.println("Anzahl der gelöschten Datensätze: " + affectedRows);
+            } catch (SQLException ex)
+            {
+                System.out.println("Fehler im SQL-DELETE Statement" + ex.getMessage());
+            }
+        } catch (SQLException e)
+        {
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+
+
+    }
     public static void updateStudentDemo()
     {
         System.out.println("Update DEMO");
